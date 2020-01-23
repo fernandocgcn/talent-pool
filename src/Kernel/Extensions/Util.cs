c349fdfef9b7a28f79ac08d712aae25bcb8044cd
Fernando Cavalcante Neto <fernando@workmail.com>
2020-01-23 08:58:15 -0300

    Inclusão da Biblioteca NetCore Utilitária

---
+++ src/Kernel/Extensions/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Resources;
+
+namespace Kernel.Extensions
+{
+    public static class Util
+    {
+        public static bool IsNullOrEmpty<T>(this T[] array)
+        {
+            return array == null || array.Length < 1;
+        }
+
+        public static bool ValidateItem<T>(this T[] array, T item, bool include)
+        {
+            bool itemExists = Array.Exists(array, element => element.Equals(item));
+            return (include && itemExists) || (!include && !itemExists);
+        }
+
+        public static string GetMessage(this Type resource, string key, params object[] arguments)
+        {
+            ResourceManager Messages = new ResourceManager(resource);
+            return Messages == null || string.IsNullOrEmpty(key)
+                ? key : string.Format(Messages.GetString(key) ?? key, arguments);
+        }
+    }
+}
