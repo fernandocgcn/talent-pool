ab490f249dcffc9d4eaf81422de10ae3ab77bb9b
Fernando Cavalcante Neto <fernando@workmail.com>
2020-01-26 22:01:22 -0300

    Inclusão dos Serviços para a Entidade Developer

---
+++ src/TPDomain/Services/DeveloperService.cs
@@ -0,0 +1,43 @@
+﻿using EntityFramework.Data;
+using TPDomain.Models;
+using System.Collections.Generic;
+
+namespace TPDomain.Services
+{
+    public class DeveloperService : IDeveloperService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeveloperService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<Developer> GetDevelopers()
+        {
+            return _unitOfWork.GetAll<Developer>();
+        }
+
+        public int Delete(Developer developer)
+        {
+            _unitOfWork.Delete(developer);
+            return _unitOfWork.Commit();
+        }
+
+        public int New(Developer developer)
+        {
+            return Save(developer, true);
+        }
+
+        public int Update(Developer developer)
+        {
+            return Save(developer, false);
+        }
+
+        private int Save(Developer developer, bool isNew)
+        {
+            _unitOfWork.Save(developer, isNew);
+            return _unitOfWork.Commit();
+        }
+    }
+}
---
+++ src/TPDomain/Services/IDeveloperService.cs
@@ -0,0 +1,13 @@
+﻿using TPDomain.Models;
+using System.Collections.Generic;
+
+namespace TPDomain.Services
+{
+    public interface IDeveloperService
+    {
+        List<Developer> GetDevelopers();
+        int Delete(Developer developer);
+        int New(Developer developer);
+        int Update(Developer developer);
+    }
+}
