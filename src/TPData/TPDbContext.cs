429a903882897e831b117bf9680b843e875efdd6
Fernando Cavalcante Neto <fernando@workmail.com>
2020-02-11 13:06:54 -0300

    Simplificação do Contexto do Banco do Projeto

--- src/TPData/TPDbContext.cs
+++ src/TPData/TPDbContext.cs
@@ -1,5 +1,4 @@
-﻿using TPDomain.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace TPData
@@ -13,13 +12,5 @@ namespace TPData
             modelBuilder.HasDefaultSchema("dbo");
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
-
-        public DbSet<Availability> Availabilities { get; set; }
-        public DbSet<WorkingTime> WorkingTimes { get; set; }
-        public DbSet<Knowledge> Knowledges { get; set; }
-        public DbSet<Developer> Developers { get; set; }
-        public DbSet<DeveloperAvailability> DeveloperAvailabilities { get; set; }
-        public DbSet<DeveloperWorkingTime> DeveloperWorkingTimes { get; set; }
-        public DbSet<DeveloperKnowledge> DeveloperKnowledges { get; set; }
     }
 }
